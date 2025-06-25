1ff1ac831effd5f1b016e982ff66e15dd18944cd
Kokopw <kmelon175@gmail.com>
2025-06-25 11:29:29 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1403,18 +1403,18 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
                 InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='buy_info')
             ]]
-        reply_markup = InlineKeyboardMarkup(btn)
-        await client.edit_message_media(                
-            chat_id=query.message.chat.id, 
-            message_id=query.message.id, 
-            media=InputMediaPhoto("https://files.catbox.moe/ibmrjz.jpg"),  # <- Image URL added here
-            reply_markup=reply_markup,
-        )
-        await query.message.edit_caption(
-            caption=script.PREMIUM_UPI_TEXT.format(OWNER_UPI_ID),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+            reply_markup = InlineKeyboardMarkup(btn)
+            await client.edit_message_media(                
+                chat_id=query.message.chat.id, 
+                message_id=query.message.id, 
+                media=InputMediaPhoto("https://files.catbox.moe/ibmrjz.jpg"),  # <- Image URL added here
+                reply_markup=reply_markup,
+            )
+            await query.message.edit_caption(
+                caption=script.PREMIUM_UPI_TEXT.format(OWNER_UPI_ID),
+                reply_markup=reply_markup,
+                parse_mode=enums.ParseMode.HTML
+            )
         except Exception as e:
             logging.exception("Exception in 'upi' callback")
 
